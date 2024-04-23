ae1f738256029ba39b2d2cca228077904ca6788b
DonCanjas <80267631+DonCanjas@users.noreply.github.com>
2024-04-23 02:39:37 +0100

    Update 7z-cryptor.py

--- 7z-cryptor.py
+++ 7z-cryptor.py
@@ -1,4 +1,4 @@
-import os, sys, argparse
+import os, sys, argparse, py7zr
 from py7zr import SevenZipFile
 from py7zr.exceptions import PasswordRequired
 
@@ -25,7 +25,7 @@ def decompress_7z(source_path, destination_path, password=None, subfolder=False)
             except Exception as e:
                 print(f"Error extracting {file_path}: {e}")
 
-def compress_7z(source_path, destination_path, password=None, subfolder=False, delete_original=False):
+def compress_7z(source_path, destination_path, password=None, subfolder=False, delete_original=False, header_encryption=True, algorithm='COPY'):
     if not subfolder:
         print("Subfolder option is disabled. Subfolders will not be processed.")
 
@@ -38,7 +38,22 @@ def compress_7z(source_path, destination_path, password=None, subfolder=False, d
                 try:
                     archive_name = os.path.splitext(file_path)[0] + ".7z"
                     
-                    with SevenZipFile(archive_name, mode='w', password=password) as z:
+                    # Dictionary mapping compression algorithm names to filter IDs
+                    filters_map = {
+                        'COPY': py7zr.FILTER_COPY,
+                        'LZMA2': py7zr.FILTER_LZMA2,
+                        'LZMA': py7zr.FILTER_LZMA,
+                        'Bzip2': py7zr.FILTER_BZIP2,
+                        'Deflate': py7zr.FILTER_DEFLATE,
+                        'PPMd': py7zr.FILTER_PPMD,
+                        'ZStandard': py7zr.FILTER_ZSTD,
+                        'Brotli': py7zr.FILTER_BROTLI
+                    }
+
+                    # Set compression filters based on the chosen algorithm
+                    filters = [{"id": filters_map.get(algorithm)}]
+
+                    with SevenZipFile(archive_name, mode='w', password=password, filters=filters, header_encryption=header_encryption) as z:
                         rel_path = os.path.relpath(file_path, source_path)
                         z.write(file_path, arcname=rel_path)
                         print(f"Compression of {file_path} completed successfully.")
@@ -53,7 +68,7 @@ def compress_7z(source_path, destination_path, password=None, subfolder=False, d
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                     prog="7z-cryptor",
-                    description="Encrypts/Decrytps 7z files within a set path. Repo: https://github.com/DonCanjas/7z-cryptor")
+                    description="Encrypts/Decrypts 7z files within a set path. Repo: https://github.com/DonCanjas/7z-cryptor")
 
     # Add arguments
     parser.add_argument('-d', '--decompress', metavar='DECOMPRESS_INPUT', nargs='?', const=os.getcwd(), help='Path of 7z archives to decompress (default: current directory)') 
@@ -62,6 +77,9 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--password', metavar='PASSWORD', action='store', help='Password to encrypt/decrypt 7z files')
     parser.add_argument('-sub', '--subfolder', action='store_true', help='Process subfolders (default: disabled)')
     parser.add_argument('-del', '--delete', dest='delete_original', action='store_true', help='Delete original files after encryption/decryption (default: disabled)')
+    parser.add_argument('-no-he', '--no-header-encryption', dest='header_encryption', action='store_false', help='Disable header encryption')
+    parser.add_argument('-algo', '--algorithm', metavar='ALGORITHM', choices=['COPY', 'LZMA2', 'LZMA', 'Bzip2', 'Deflate', 'PPMd', 'ZStandard', 'Brotli'], default='COPY', 
+    help='Compression algorithm (default: COPY). Available alorithms: COPY, LZMA2, LZMA, Bzip2, Deflate, PPMd (depend on pyppmd), ZStandard (depend on pyzstd), Brotli (depend on brotli, brotliCFFI)')
     args = parser.parse_args()
 
     if len(sys.argv)==1:
@@ -81,9 +99,11 @@ if __name__ == "__main__":
         destination_path = args.output if args.output else source_path
         password = args.password
         subfolder = args.subfolder
+
         if not password:
             print("Please specify a password for compression with -p or --password")
             sys.exit(1)
-        compress_7z(source_path, destination_path, password, subfolder, args.delete_original)
+
+        compress_7z(source_path, destination_path, password, subfolder, args.delete_original, args.header_encryption, args.algorithm)
     else:
         print("No job was defined.")
